b8b838fa7c754f91cb756d4e89ee8c1da3e41123
rutik321mi <76554795+rutik321mi@users.noreply.github.com>
2022-08-01 16:29:03 +0530

    Update RemoveStarLeftRightChar.java

--- Assignment day 4/RemoveStarLeftRightChar.java
+++ Assignment day 4/RemoveStarLeftRightChar.java
@@ -1,3 +1,9 @@
+/*Write a Java program that accepts a string (with * in it). The program should return a new string 
+in which the following characters are removed-*, the characters that are to the left and right of *
+Example1)
+i/p:ab*cd
+o/p:ad
+*/
 package assignment4;
 
 import java.util.Scanner;
