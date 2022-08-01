4b459319d352ebdd3026d74876fb159e9a11197a
rutik321mi <76554795+rutik321mi@users.noreply.github.com>
2022-08-01 16:16:54 +0530

    Update Orange.java

--- Assignment day 3/Orange.java
+++ Assignment day 3/Orange.java
@@ -1,3 +1,9 @@
+/*Create  a base class Fruit with name ,taste and size as its attributes. 
+Create a method called eat() which describes the name of the fruit and its taste. 
+Inherit the same in 2 other classes Apple and Orange and override the eat() method to represent each fruit taste.
+*/
+
+
 class Fruit{
 	String name,taste;
 	int size;
