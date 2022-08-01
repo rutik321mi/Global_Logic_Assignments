0746b90b34ae7773ca46526ab67f72b1be182ce6
rutik321mi <76554795+rutik321mi@users.noreply.github.com>
2022-08-01 16:22:19 +0530

    Update FirstAndLastCharRemove.java

--- Assignment day 4/FirstAndLastCharRemove.java
+++ Assignment day 4/FirstAndLastCharRemove.java
@@ -1,3 +1,10 @@
+/*Write a java program that accepts a string and 
+returns a new string without the first and last character of the input string.
+Example1)
+i/p:Suman
+o/p:uma
+*/
+
 package assignment4;
 
 import java.util.Scanner;
