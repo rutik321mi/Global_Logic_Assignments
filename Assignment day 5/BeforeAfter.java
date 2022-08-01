9892394ee0ef9642879f5c20d858784e8fc8f389
rutik321mi <76554795+rutik321mi@users.noreply.github.com>
2022-08-01 16:33:21 +0530

    Update BeforeAfter.java

--- Assignment day 5/BeforeAfter.java
+++ Assignment day 5/BeforeAfter.java
@@ -1,3 +1,19 @@
+/*Given two strings a and b, return a new string, following the rules given below. 
+If string b occurs in string a, then the new string should concatenate the characters that appear before 
+and after of String b.
+Ignore cases where there is no character before or after the word, 
+and a character may be included twice if it is in between two string b's.
+Example1)
+i/p:abcXY123XYijk,XY
+o/p:c13i
+Example2)
+i/p:XY123XY,XY
+o/p:13
+Example3)
+i/p:XY1XY,XY
+o/p:11
+*/
+
 import java.util.*;
 public class BeforeAfter {
 
