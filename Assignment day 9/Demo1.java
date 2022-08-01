98f852e3bbee9ada5dd9dbf49c719e084d401aa7
rutik321mi <76554795+rutik321mi@users.noreply.github.com>
2022-08-01 17:00:15 +0530

    Update Demo1.java

--- Assignment day 9/Demo1.java
+++ Assignment day 9/Demo1.java
@@ -1,3 +1,7 @@
+/*Class Name: Demo1
+Method: String stringConcat(String,String)  [ returns concatenation of the 2 Strings received ]
+Create the above class and method and test it using JUnit.
+*/
 
 public class Demo1 {
 String stringConcat(String s1,String s2) {
